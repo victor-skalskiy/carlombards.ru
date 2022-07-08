23725b21769000afa256de0307dd753d6438e5d9
dasp <victor@skalskiy.net>
2022-07-08 20:52:53 +0300

    first commit

---
+++ CarLombards/Controllers/ArticleController.cs
@@ -0,0 +1,111 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CarLombards.Models;
+
+namespace CarLombards.Controllers;
+
+public class ArticleController : Controller
+{
+    private readonly ILogger<ArticleController> _logger;
+
+    public ArticleController(ILogger<ArticleController> logger)
+    {
+        _logger = logger;
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Route("analytics")]
+    public IActionResult Analytics()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [Route("articles/svodnaya-tablitsa-shem-avtolombardov")]
+    public IActionResult Scheme()
+    {
+        return View();
+    }
+
+    [Route("articles")]
+    public IActionResult List()
+    {
+        return View();
+    }
+
+    [Route("articles/finance-simulation")]
+    public IActionResult Simulation()
+    {
+        return View();
+    }
+
+    [Route("articles/kakie-nuzhni-sotrudniki-v-avtolombard")]
+    public IActionResult Staff()
+    {
+        return View();
+    }
+
+    [Route("articles/oshibki-pri-otkrytii-avtolombarda")]
+    public IActionResult OpenedErrors()
+    {
+        return View();
+    }
+
+    [Route("articles/finansovye-pokazateli-po-periodam")]
+    public IActionResult FinCounters()
+    {
+        return View();
+    }
+
+    [Route("articles/pomosch-pri-otkrytii-lombarda")]
+    public IActionResult Help()
+    {
+        return View();
+    }
+
+    [Route("articles/primery-paketov-dokumentov")]
+    public IActionResult DocsExample()
+    {
+        return View();
+    }
+
+    [Route("articles/voprosi-i-otvety")]
+    public IActionResult FAQ()
+    {
+        return View();
+    }
+
+    [Route("articles/community-avtolombardov")]
+    public IActionResult Community()
+    {
+        return View();
+    }
+
+    [Route("about")]
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    [Route("articles/otchetnost-i-nadzornye-organy")]
+    public IActionResult Reports()
+    {
+        return View();
+    }
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# carlombards.ru
